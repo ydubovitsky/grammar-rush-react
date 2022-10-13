a8605c61c277f99aadd758f2f736f010c111f53e
user <y.dubovitsky@gmail.com>
2022-10-13 18:02:49 +0300

    config added for modules declaration

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,6 @@
+declare module ".css"
 declare module "*.module.css";
 declare module "*.module.scss";
 declare module '*.jpg';
+declare module '*.png';
 // and so on for whatever flavor of css you're using
