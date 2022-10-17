d0ba42a97cfcb0a1037bc9f23772dcbe024a0551
user <y.dubovitsky@gmail.com>
2022-10-17 12:20:00 +0300

    AboutUsComponent added

---
+++ src/pages/main/components/about-us/about-us.component.tsx
@@ -0,0 +1,9 @@
+import styles from './about-us.module.css';
+
+const AboutUsComponent:React.FC = () => {
+  return (
+    <div className={styles.container}></div>
+  )
+}
+
+export default AboutUsComponent;
