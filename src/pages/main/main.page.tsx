742fb2ca2001ff686c504a7d91e243835079ba45
user <y.dubovitsky@gmail.com>
2022-10-17 12:22:19 +0300

    MainPage added

--- src/pages/main/main.page.tsx
+++ src/pages/main/main.page.tsx
@@ -1,12 +1,20 @@
 import styles from "./main.module.css";
+import HeroComponent from "./components/hero/hero.component";
 import TaskListComponent from "./components/task-list/task-list.component";
+import FeaturesComponent from "./components/features/features.component";
+import DownloadComponent from "./components/download/download.component";
+import AboutUsComponent from "./components/about-us/about-us.component";
 
 const MainPage: React.FC = () => {
   return (
     <div className={styles.container}>
+      <HeroComponent />
+      <FeaturesComponent />
+      <DownloadComponent />
+      <AboutUsComponent />
       <TaskListComponent />
     </div>
-  )
+  );
 }
 
 export default MainPage;
