3543372eef7b1060ddfaf417f4afa4fcacfd74c3
user <y.dubovitsky@gmail.com>
2022-10-26 11:08:27 +0300

    Menu component moved to a separate component

--- src/pages/main/components/hero/hero.component.tsx
+++ src/pages/main/components/hero/hero.component.tsx
@@ -1,21 +1,11 @@
-import React, { useRef } from 'react';
-import { Link } from 'react-router-dom';
-import AnimatedLogoAtomicComponent from '../../../../common/atomic-components/animated-logo/animated-logo.component';
-import ButtonComponent from '../../../../common/components/button/button.component';
-import styles from './hero.module.css';
-import image from './images/ipad-hand.png';
+import React from "react";
+import AnimatedLogoAtomicComponent from "../../../../common/atomic-components/animated-logo/animated-logo.component";
+import ButtonComponent from "../../../../common/components/button/button.component";
+import styles from "./hero.module.css";
+import image from "./images/ipad-hand.png";
+import MenuComponent from "./menu/menu.component";
 
 const HeroComponent: React.FC = () => {
-
-  const menuElementRef = useRef<HTMLDivElement>(null);
-
-  function menuToggleHandler(e: React.MouseEvent<HTMLInputElement>): void {
-    const bodyEl = document.querySelector("body");
-    bodyEl!.style.overflow = (bodyEl!.style.overflow == 'hidden') ? 'visible' : 'hidden';
-    const menuEl = menuElementRef.current;
-    menuEl!.classList.toggle(styles.active);
-  }
-
   return (
     <div className={styles.container}>
       <div className={styles.leftColumn}>
@@ -34,17 +24,10 @@ const HeroComponent: React.FC = () => {
         <div className={styles.circleButton}>
           <i className="fas fa-chevron-left"></i>
         </div>
-        <i
-          onClick={menuToggleHandler}
-          className={`fas fa-bars ${styles.barsBtn}`}
-        ></i>
       </div>
-      <div ref={menuElementRef} className={styles.menu}>
-        <Link to={"/main/auth"} className={styles.menuBtn}>Sign In</Link>
-        <h1 onClick={menuToggleHandler} className={styles.menuBtn}>Exit</h1>
-      </div>
+      <MenuComponent />
     </div>
   );
-}
+};
 
 export default HeroComponent;
---
+++ src/pages/main/components/hero/menu/menu.component.tsx
@@ -0,0 +1,53 @@
+import React, { useRef } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { authStatusSelector } from "../../../../../redux/features/auth/auth.slice";
+import { FETCH_STATUS } from "../../../../../redux/types";
+import styles from "./menu.module.css";
+
+const MenuComponent: React.FC = () => {
+  const menuElementRef = useRef<HTMLDivElement>(null);
+  const status = useSelector(authStatusSelector);
+
+  //TODO Упростить
+  const menuToggleHandler = (e: React.MouseEvent<HTMLInputElement>): void => {
+    const bodyEl = document.querySelector("body");
+    bodyEl!.style.overflow =
+      bodyEl!.style.overflow === "hidden" ? "visible" : "hidden";
+    const menuEl = menuElementRef.current;
+    menuEl!.classList.toggle(styles.active);
+  };
+
+  const showMenuElements = (status: string) => {
+    if (status === FETCH_STATUS.AUTHENTICATED) {
+      return (
+        <Link to={"dashboard"} className={styles.menuBtn}>
+          Dashboard
+        </Link>
+      );
+    } else {
+      return (
+        <Link to={"/main/auth"} className={styles.menuBtn}>
+          Sign In
+        </Link>
+      );
+    }
+  };
+
+  return (
+    <>
+      <i
+        onClick={menuToggleHandler}
+        className={`fas fa-bars ${styles.barsBtn}`}
+      ></i>
+      <div ref={menuElementRef} className={styles.menu}>
+        {showMenuElements(status)}
+        <h1 onClick={menuToggleHandler} className={styles.menuBtn}>
+          Close
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default MenuComponent;
