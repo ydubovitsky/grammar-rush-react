b93034436281d7738d449a7cdadb98a023c2c000
user <y.dubovitsky@gmail.com>
2022-11-01 13:04:07 +0300

    temporary fix of overflowvisible to dashboard page added

--- src/pages/dashboard/dashboard.page.tsx
+++ src/pages/dashboard/dashboard.page.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import HeaderComponent from "./components/header/header.component";
 import styles from "./dashboard.module.css";
 
 const DashboardPage = () => {
+
+  //TODO Убрать?
+  const setOverflowVisible = (): void => {
+    const bodyEl = document.querySelector("body");
+    bodyEl!.style.overflow = "visible";
+  };
+
+  useEffect(() => {
+    setOverflowVisible();
+  }, []);
+
   return (
     <div className={styles.container}>
       <HeaderComponent />
