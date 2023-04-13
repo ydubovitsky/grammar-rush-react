da37656f7b5ce24cc297736a257977d4cbf1adbe
user <y.dubovitsky@gmail.com>
2023-04-13 15:36:49 +0300

    const BASE_URL equals process.env or placeholder for now

--- src/redux/const/url-endpoints.ts
+++ src/redux/const/url-endpoints.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8085';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL != null ? process.env.REACT_APP_BACKEND_URL : "http://localhost:8080";
 export const GITHUB_URL = 'https://api.github.com';
