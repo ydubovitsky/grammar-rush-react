1bef945b336db43abe0ad5d35fb180c8103d3864
user <y.dubovitsky@gmail.com>
2023-10-26 14:23:39 +0300

    HeroComponents uses updated Spline link

--- src/pages/main/components/hero/hero.component.tsx
+++ src/pages/main/components/hero/hero.component.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import MobileApp3dComponent from "../../../../common/components/mobile-app-3d/mobile-app-3d.component";
 import styles from "./hero.module.css";
 
-const SPLINE_URL =
-  "https://prod.spline.design/zepWzaKNvJWmV3dB/scene.splinecode";
+const SPLINE_URL = "https://prod.spline.design/zepWzaKNvJWmV3dB/scene.splinecode";
 
 const HeroComponent: React.FC = (): JSX.Element => {
   return (
